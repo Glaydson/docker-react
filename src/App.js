6399e40cb9fa13e3afa4b98b9395879b2388b30e
Glaydson <glaydsonvs@gmail.com>
2021-07-13 19:47:01 -0300

    Commit de teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite o App.js e salve para recarregar
         </p>
         <a
           className="App-link"
